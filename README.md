cf8d0e971e6aa7634b3b2a26608ee6089da68fcf
neil <nilnil47@gmail.com>
2020-04-15 17:11:50 +0300

    change the video source to be udp stream and remove the native modules from the app

---
+++ README.md
@@ -0,0 +1,3 @@
+## forked from: https://code.videolan.org/videolan/libvlc-android-samples
+
+
--- java_sample/src/main/java/org/videolan/javasample/JavaActivity.java
+++ java_sample/src/main/java/org/videolan/javasample/JavaActivity.java
@@ -10,7 +10,9 @@
 
 package org.videolan.javasample;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,7 +26,7 @@ import java.util.ArrayList;
 
 public class JavaActivity extends AppCompatActivity  {
     private static final boolean USE_TEXTURE_VIEW = false;
-    private static final boolean ENABLE_SUBTITLES = true;
+    private static final boolean ENABLE_SUBTITLES = false;
     private static final String ASSET_FILENAME = "bbb.m4v";
 
     private VLCVideoLayout mVideoLayout = null;
@@ -35,11 +37,15 @@ public class JavaActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         final ArrayList<String> args = new ArrayList<>();
         args.add("-vvv");
+
+        // this should improve the:
+        // "video output: picture might be displayed late" debug message
+        args.add(":codec=mediacodec_ndk,mediacodec_jni,none");
+
         mLibVLC = new LibVLC(this, args);
         mMediaPlayer = new MediaPlayer(mLibVLC);
 
@@ -59,15 +65,26 @@ public class JavaActivity extends AppCompatActivity  {
         super.onStart();
 
         mMediaPlayer.attachViews(mVideoLayout, null, ENABLE_SUBTITLES, USE_TEXTURE_VIEW);
+        final Media media = new Media(mLibVLC, Uri.parse("udp://@:1111"));
 
-        try {
-            final Media media = new Media(mLibVLC, getAssets().openFd(ASSET_FILENAME));
-            mMediaPlayer.setMedia(media);
-            media.release();
-        } catch (IOException e) {
-            throw new RuntimeException("Invalid asset folder");
-        }
+        // this line enable the hardware acceleration for the media
+        // it should improve the decoding process but it does not seems to do en effect
+        media.setHWDecoderEnabled(true, true);
+
+
+        mMediaPlayer.setMedia(media);
+
+        media.release();
         mMediaPlayer.play();
+
+//        try {
+//            final Media media = new Media(mLibVLC, getAssets().openFd(ASSET_FILENAME));
+//            mMediaPlayer.setMedia(media);
+//            media.release();
+//        } catch (IOException e) {
+//            throw new RuntimeException("Invalid asset folder");
+//        }
+//        mMediaPlayer.play();
     }
 
     @Override
